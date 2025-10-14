032a858de47a0a7ab7ba1198f820b9917c7140f2
ENG_BXI <eng.0.bxi@gmail.com>
2025-10-14 17:06:37 +0300

    add back navigate to subdomain

--- src/Features/DeptForCompaniesFeature/Pages/DeptDetails.tsx
+++ src/Features/DeptForCompaniesFeature/Pages/DeptDetails.tsx
@@ -5,7 +5,7 @@ import {PayDeptDialogContent} from '../Components/PayDeptDialog';
 const DeptDetails = () => {
   return (
     <section>
-      <SectionTitle title='دفعات دين 40000' hasButton isButtonOpenDialog buttonText='تسديد دين' dialogContent={<PayDeptDialogContent />} />
+      <SectionTitle title='دفعات دين 40000' hasButton hasBackButton isButtonOpenDialog buttonText='تسديد دين' dialogContent={<PayDeptDialogContent />} />
       <AllDeptDetailsTable />
     </section>
   );
